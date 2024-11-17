b53bd9fcffc3c803c8d4eed93f8102507680f7e8
[ ] <36614790+UnZero2126@users.noreply.github.com>
2024-11-17 17:47:20 +0800

    /added new lines of code in button

--- TestProject/TestProject/Form1.vb
+++ TestProject/TestProject/Form1.vb
@@ -1,3 +1,6 @@
 ﻿Public Class Form1
-
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        ' THIS IS A TEST LINE
+        MessageBox.Show("Hello, this is a git test. ")
+    End Sub
 End Class
